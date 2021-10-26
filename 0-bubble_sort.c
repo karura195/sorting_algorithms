538f34bc41bfae40b229808e8bdbd3d9c7b193c6
karura195 <karla.diaz@pucp.pe>
2021-10-25 18:21:05 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,11 @@ void bubble_sort(int *array, size_t size)
 {
 	int i, j, a;
 
+	if (sizeof(array) == 0 || size <= 1)
+	{
+		return;
+	}
+
 	for(i = 0; i < size - 1; i++)
 	{
 		for(j = 0; j < size - 1 - i; j++)
